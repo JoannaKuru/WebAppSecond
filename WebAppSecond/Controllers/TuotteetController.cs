82c003458b4d5130957fe25319ac13af45147e65
JoannaKuruStudent <joanna.kuru@student.careeria.fi>
2020-12-21 11:32:40 +0200

    Add project files.

---
+++ WebAppSecond/Controllers/TuotteetController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebAppSecond.Models;
+
+namespace WebAppSecond.Controllers
+{
+    public class TuotteetController : Controller
+    {
+        private TilausDBEntities db = new TilausDBEntities();
+
+        // GET: Tuotteet
+        //[HttpGet]
+        public ActionResult Index()
+        {
+            if (Session["UserName"] == null)
+            {
+                ViewBag.LoggedStatus = "Out";
+            }
+            else ViewBag.LoggedStatus = "In";
+
+            if (Session["UserName"] == null)
+            {
+                return RedirectToAction("login", "home");
+            }
+            else
+            {
+                TilausDBEntities db = new TilausDBEntities();
+                List<Tuotteet> model = db.Tuotteet.ToList();
+                db.Dispose();
+                return View(model);
+            }
+        }
+
+        // GET: Tuotteet/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tuotteet tuotteet = db.Tuotteet.Find(id);
+            if (tuotteet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tuotteet);
+        }
+
+        // GET: Tuotteet/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tuotteet/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TuoteID,Nimi,Ahinta,Kuva")] Tuotteet tuotteet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tuotteet.Add(tuotteet);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tuotteet);
+        }
+
+        // GET: Tuotteet/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tuotteet tuotteet = db.Tuotteet.Find(id);
+            if (tuotteet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tuotteet);
+        }
+
+        // POST: Tuotteet/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TuoteID,Nimi,Ahinta,Kuva")] Tuotteet tuotteet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tuotteet).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tuotteet);
+        }
+
+        // GET: Tuotteet/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tuotteet tuotteet = db.Tuotteet.Find(id);
+            if (tuotteet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tuotteet);
+        }
+
+        // POST: Tuotteet/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tuotteet tuotteet = db.Tuotteet.Find(id);
+            db.Tuotteet.Remove(tuotteet);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
